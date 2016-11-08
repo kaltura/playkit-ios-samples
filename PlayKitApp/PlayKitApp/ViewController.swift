a03cab716fd5bd45a71fec7defe2d70427bc1053
Eliza Sapir <eliza.sapir@kaltura.com>
2016-11-08 18:16:42 +0200

    resolve issues

--- PlayKitApp/PlayKitApp/ViewController.swift
+++ PlayKitApp/PlayKitApp/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import PlayKit
 
 class ViewController: UIViewController {
-
+    var player : Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        PlayKitManager.sharedInstance.registerPlugin(SamplePlugin.self)
+        let config = PlayerConfig()
+        self.player = PlayKitManager.sharedInstance.createPlayer(config:config)
+        
+        self.player.layer.backgroundColor = UIColor.red.cgColor
+        self.player.layer.frame = playerContainer.bounds
+        self.playerContainer.layer.addSublayer(player.layer)
+        self.player.load(config)
     }
 
+    @IBOutlet weak var playerContainer: UIView!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+      //  self.player.play()
+    }
+    @IBAction func playClicked(_ sender: AnyObject) {
+        self.player.play()
+    }
 }
 
