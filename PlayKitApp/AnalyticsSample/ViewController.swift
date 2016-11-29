7e2ed27e8328dc962a2b522547b24f374dc96bd2
Oded Klein <Oded.Klein@kaltura.com>
2016-11-29 15:09:47 +0200

    Work on analytics sample app

--- PlayKitApp/AnalyticsSample/ViewController.swift
+++ PlayKitApp/AnalyticsSample/ViewController.swift
@@ -9,23 +9,30 @@
 import UIKit
 import PlayKit
 import SwiftyJSON
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var playerController: Player!
 
     @IBOutlet weak var playerView: UIView!
-    
+    @IBOutlet weak var progressBar: UISlider!
+    @IBOutlet weak var durationTimeText: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        PlayKitManager.sharedInstance.registerPlugin(YouboraPlugin.self)
 
         setupVideo()
     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let controller = self.playerController {
+            controller.view.frame = CGRect(origin: CGPoint.zero, size: playerView.frame.size)
+        }
     }
 
     func setupVideo() {
@@ -57,16 +64,29 @@ class ViewController: UIViewController {
         self.playerController = PlayKitManager.sharedInstance.loadPlayer(config: config)
         playerView.addSubview(self.playerController.view)
 
-        
+        self.playerController.addObserver(self, events: [PlayerEvents.canPlay.self, PlayerEvents.play.self], block: {(info) in
+            
+            PKLog.debug("Duration: \(self.playerController.duration)")
+            
+            })
     }
     
     @IBAction func playClicked(_ sender: AnyObject) {
         self.playerController.play()
     }
     
+    @IBAction func pauseClicked(_ sender: AnyObject) {
+        self.playerController.pause()
+    }
     
-    
-    
+    @IBAction func playheadValueChanged(_ sender: AnyObject) {
+        if (!sender.isKind(of: UISlider.self)) {
+            return
+        }
+        let slider = sender as! UISlider
+        playerController.seek(to: CMTimeMake(Int64(slider.value), 1))
+    }
+
     
 }
 
