06a9e08cc6ea0d43b6a1498cc3148507dd921d81
Eliza Sapir <eliza.sapir@kaltura.com>
2017-03-20 16:55:06 +0200

    register each event separatly

--- EventsRegistration/BasicSample/ViewController.m
+++ EventsRegistration/BasicSample/ViewController.m
@@ -26,10 +26,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // setup our player instance
-    [self setupPlayer];
-    // register player events
-    [self registerPlayerEvents:[self playerEvents]];
+    // Load Player
+    NSError *error = [self loadPlayer];
+    // Register Player Events
+    // >Note: Make sure to register befor `prepare` is called
+    //        Otherwise you will miss events
+    [self registerPlayerEvents];
+    // Prepare Player
+    [self preparePlayer:error];
 }
  
 - (void)viewDidLayoutSubviews {
@@ -40,8 +44,15 @@
 /*********************************/
 #pragma mark - Player Setup
 /*********************************/
+- (NSError *)loadPlayer {
+    // load the player
+    NSError *error = nil;
+    self.player = [PlayKitManager.sharedInstance loadPlayerWithPluginConfig:nil error:&error];
     
-- (void)setupPlayer {
+    return error;
+}
+
+- (void)preparePlayer:(NSError *)error {
     NSURL *contentURL = [[NSURL alloc] initWithString:@"https://cdnapisec.kaltura.com/p/2215841/playManifest/entryId/1_w9zx2eti/format/applehttp/protocol/https/a.m3u8"];
     
     // create media source and initialize a media entry with that source
@@ -54,39 +65,64 @@
     // create media config
     MediaConfig *mediaConfig = [[MediaConfig alloc] initWithMediaEntry:mediaEntry startTime:0.0];
     
-    // load the player
-    NSError *error = nil;
-    self.player = [PlayKitManager.sharedInstance loadPlayerWithPluginConfig:nil error:&error];
-    
     if (!error) {
+        // prepare the player
         [self.player prepare:mediaConfig];
         [self.playerContainer addSubview:self.player.view];
     } else {
         // error loading the player
+        NSLog(@"error:: %@",error.description);
     }
 }
 
 /*********************************/
 #pragma mark - Events Registration
 /*********************************/
-- (NSArray *)playerEvents {
-    return @[PlayerEvent.playing,
-             PlayerEvent.durationChanged,
-             PlayerEvent.stateChanged];
+- (void)registerPlayerEvents {
+    [self registerPlayEvent];
+    [self registerDurationChangedEvent];
+    [self registerPlayerEventStateChangedEvent];
+    [self registerErrorEvent];
 }
 
-- (void)registerPlayerEvents:(NSArray *)events {
-    // add observer to list of different events
-    [self.player addObserver:self events:events block:^(PKEvent * _Nonnull event) {
-        if ([event isKindOfClass:PlayerEvent.playing]) {
-            NSLog(@"Playing Event");
-        } else if ([event isKindOfClass:PlayerEvent.durationChanged]) {
-            NSNumber *duration = event.duration;
-            NSLog(@"Duration Changed Event: %@", duration);
-        } else if ([event isKindOfClass:PlayerEvent.stateChanged]) {
-            PlayerState oldState = event.oldState;
-            PlayerState newState = event.newState;
-            NSLog(@"State Chnaged Event:: oldState: %d | newState: %d", (int)oldState, (int)newState);
+// Handle basic event (Play, Pause, CanPlay ..)
+- (void)registerPlayEvent {
+    [self.player addObserver:self
+                      events:@[PlayerEvent.playing]
+                       block:^(PKEvent * _Nonnull event) {
+        NSLog(@"Playing Event");
+    }];
+}
+
+// Handle Duration Changes
+- (void)registerDurationChangedEvent {
+    [self.player addObserver:self
+                      events:@[PlayerEvent.durationChanged]
+                       block:^(PKEvent * _Nonnull event) {
+                           if (event.duration) {
+                               NSNumber *duration = event.duration;
+                               NSLog(@"Duration Changed Event: %@", duration);
+                           }
+                       }];
+}
+
+// Handle State Changes
+- (void)registerPlayerEventStateChangedEvent {
+    [self.player addObserver:self
+                      events:@[PlayerEvent.stateChanged]
+                       block:^(PKEvent * _Nonnull event) {
+                           PlayerState oldState = event.oldState;
+                           PlayerState newState = event.newState;
+                           NSLog(@"State Chnaged Event:: oldState: %d | newState: %d", (int)oldState, (int)newState);
+                       }];
+}
+
+// Handle Player Errors
+- (void)registerErrorEvent {
+    [self.player addObserver:self events:@[PlayerEvent.error] block:^(PKEvent * _Nonnull event) {
+        NSError *error = event.error;
+        if (error && error.code == PKErrorCode.AssetNotPlayable) {
+            // handle error
         }
     }];
 }
@@ -94,7 +130,7 @@
 /*********************************/
 #pragma mark - Actions
 /*********************************/
-    
+
 - (IBAction)playTouched:(UIButton *)sender {
     if(!self.player.isPlaying) {
         self.playheadTimer = [NSTimer scheduledTimerWithTimeInterval:0.5f target:self selector:@selector(playheadUpdate) userInfo:nil repeats:YES];
