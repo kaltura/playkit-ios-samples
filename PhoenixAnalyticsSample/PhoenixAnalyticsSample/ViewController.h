cc2d4f82a40aed655c3af08c6de4265c773fd4dd
Gal Orlanczyk <gal.orlanczyk@outlook.com>
2017-03-21 16:08:21 +0200

    Separated phoenix analytics sample

---
+++ PhoenixAnalyticsSample/PhoenixAnalyticsSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoenixAnalyticsSample
+//
+//  Created by Gal Orlanczyk on 21/03/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
