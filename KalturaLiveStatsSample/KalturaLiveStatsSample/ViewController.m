2db4c1e5c47f91db238be779cb62cd3ce6d01601
Gal Orlanczyk <gal.orlanczyk@outlook.com>
2017-03-21 16:39:10 +0200

    Separated kaltura live stats plugin sample

---
+++ KalturaLiveStatsSample/KalturaLiveStatsSample/ViewController.m
@@ -0,0 +1,160 @@
+//
+//  ViewController.m
+//  KalturaLiveStatsSample
+//
+//  Created by Gal Orlanczyk on 21/03/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "PlayKit-Swift.h"
+
+@interface ViewController ()
+
+@property (strong, nonatomic) id<Player> player;
+@property (strong, nonatomic) NSTimer *playheadTimer;
+@property (weak, nonatomic) IBOutlet UIView *playerContainer;
+@property (weak, nonatomic) IBOutlet UISlider *playheadSlider;
+
+@end
+
+/*********************************/
+// Plugin registration should be done in App Delegate!!!
+// Don't forget to add it in your project.
+// Look at `AppDelegate` to see the registration.
+/*********************************/
+
+/*
+ This sample will show you how to create a player with kaltura live stats plugin.
+ The steps required:
+ 1. Create plugin config.
+ 2. Load player with plugin config.
+ 3. Register player events.
+ 4. Prepare Player.
+ */
+@implementation ViewController
+
+/*********************************/
+#pragma mark - LifeCycle
+/*********************************/
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 1. Create plugin config
+    PluginConfig *pluginConfig = [self createPluginConfig];
+    // 2. Load the player
+    NSError *error = nil;
+    self.player = [[PlayKitManager sharedInstance] loadPlayerWithPluginConfig:pluginConfig error:&error];
+    // make sure player loaded
+    if (!error) {
+        // 3. Register events if have ones.
+        // Event registeration must be after loading the player successfully to make sure events are added,
+        // and before prepare to make sure no events are missed (when calling prepare player starts buffering and sending events)
+        [self addKalturaLiveStatsObservations];
+        
+        // 4. Prepare the player (can be called at a later stage, preparing starts buffering the video)
+        [self preparePlayer];
+    } else {
+        // error loading the player
+    }
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.player.view.frame = self.playerContainer.bounds;
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    // remove observers
+    [self removeAnalyticsObservations];
+}
+
+/*********************************/
+#pragma mark - Player Setup
+/*********************************/
+
+- (void)preparePlayer {
+    NSURL *contentURL = [[NSURL alloc] initWithString:@"https://nasatv-lh.akamaihd.net/i/NASA_101@319270/master.m3u8"];
+    
+    // create media source and initialize a media entry with that source
+    NSString *entryId = @"sintel";
+    MediaSource* source = [[MediaSource alloc] init:entryId contentUrl:contentURL mimeType:nil drmData:nil mediaFormat:MediaFormatHls];
+    NSArray<MediaSource*>* sources = [[NSArray alloc] initWithObjects:source, nil];
+    
+    // setup media entry
+    MediaEntry *mediaEntry = [[MediaEntry alloc] init:entryId sources:sources duration:-1];
+    
+    // create media config
+    MediaConfig *mediaConfig = [[MediaConfig alloc] initWithMediaEntry:mediaEntry startTime:0.0];
+    
+    // prepare the player
+    [self.player prepare:mediaConfig];
+    
+    // setup the player's view
+    [self.playerContainer addSubview:self.player.view];
+    self.player.view.frame = self.playerContainer.bounds;
+}
+
+/*********************************/
+#pragma mark - Analytics
+/*********************************/
+
+// creates plugin config by adding params under the plugin name.
+- (PluginConfig *)createPluginConfig {
+    NSMutableDictionary *pluginConfigDict = [[NSMutableDictionary alloc] init];
+    pluginConfigDict[KalturaLiveStatsPlugin.pluginName] = [self createKalturaLiveStatsPluginConfig];
+    return [[PluginConfig alloc] initWithConfig:pluginConfigDict];
+}
+
+- (void)addKalturaLiveStatsObservations {
+    [self.player addObserver:self events:@[KalturaLiveStatsEvent.report] block:^(PKEvent * _Nonnull event) {
+        NSLog(@"received kaltura live stats event(buffer time): %@", event.kalturaLiveStatsBufferTime);
+    }];
+}
+
+// remove observers
+- (void)removeAnalyticsObservations {
+    [self.player removeObserver:self events:@[OttEvent.report]];
+}
+
+- (AnalyticsConfig *)createKalturaLiveStatsPluginConfig {
+    NSDictionary *kalturaLiveStatsPluginParams = @{
+                                                   @"sessionId": @"",
+                                                   @"uiconfId": @0,
+                                                   @"baseUrl": @"",
+                                                   @"partnerId": @0,
+                                                   @"timerInterval": @30
+                                                   };
+    return [[AnalyticsConfig alloc] initWithParams:kalturaLiveStatsPluginParams];
+}
+
+/*********************************/
+#pragma mark - Actions
+/*********************************/
+
+- (IBAction)playTouched:(UIButton *)sender {
+    if(!self.player.isPlaying) {
+        self.playheadTimer = [NSTimer scheduledTimerWithTimeInterval:0.5f target:self selector:@selector(playheadUpdate) userInfo:nil repeats:YES];
+        [self.player play];
+    }
+}
+
+- (IBAction)pauseTouched:(UIButton *)sender {
+    if(self.player.isPlaying) {
+        [self.playheadTimer invalidate];
+        self.playheadTimer = nil;
+        [self.player pause];
+    }
+}
+
+- (IBAction)playheadValueChanged:(UISlider *)sender {
+    NSLog(@"playhead value: %f", sender.value);
+    self.player.currentTime = self.player.duration * sender.value;
+}
+
+- (void)playheadUpdate {
+    self.playheadSlider.value = self.player.currentTime / self.player.duration;
+}
+
+@end
