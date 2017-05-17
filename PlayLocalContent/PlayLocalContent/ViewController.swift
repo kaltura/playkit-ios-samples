1c17c1f003004959a8a359ced56cedda95bb39af
Eliza Sapir <eliza.sapir@kaltura.com>
2017-05-17 09:55:43 +0300

    small updates + local content playback sample

---
+++ PlayLocalContent/PlayLocalContent/ViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  BasicSample-Swift
+//
+//  Created by Eliza Sapir on 12/04/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+import UIKit
+import PlayKit
+
+/*
+ This sample will show you how to create a player with basic functionality.
+ The steps required:
+ 1. Load player with plugin config (only if has plugins).
+ 2. Register player events.
+ 3. Prepare Player.
+ */
+
+class ViewController: UIViewController {
+    var player: Player?
+    var playheadTimer: Timer?
+    @IBOutlet weak var playerContainer: UIView!
+    @IBOutlet weak var playheadSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.playheadSlider.isContinuous = false;
+        
+        // 1. Load the player
+        do {
+            self.player = try PlayKitManager.shared.loadPlayer(pluginConfig: nil)
+            // 2. Register events if have ones.
+            // Event registeration must be after loading the player successfully to make sure events are added,
+            // and before prepare to make sure no events are missed (when calling prepare player starts buffering and sending events)
+            
+            // 3. Prepare the player (can be called at a later stage, preparing starts buffering the video)
+            self.preparePlayer()
+        } catch let e {
+            // error loading the player
+            print("error:", e.localizedDescription)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        player.view.frame = self.playerContainer.bounds
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+/************************/
+// MARK: - Player Setup
+/***********************/
+    func preparePlayer() {
+        let contentUrl = "file://\(Bundle(for: type(of: self)).path(forResource: "big_buck_bunny_short", ofType: "mp4")!)"
+  
+        // create media source and initialize a media entry with that source
+        let entryId = "sintel"
+        
+        let source = MediaSource(entryId, contentUrl: URL(string: contentUrl), drmData: nil, mediaFormat: .mp4)
+        
+        // setup media entry
+        let mediaEntry = MediaEntry(entryId, sources: [source], duration: -1)
+        
+        // create media config
+        let mediaConfig = MediaConfig(mediaEntry: mediaEntry)
+        
+        // prepare the player
+        self.player!.prepare(mediaConfig)
+        
+        // setup the player's view
+        self.playerContainer.addSubview(self.player!.view)
+        self.player!.view.frame = self.playerContainer.bounds
+    }
+    
+    lazy var assetsManager: LocalAssetsManager = {
+        return LocalAssetsManager.manager()
+    }()
+    
+    func mediaEntry(_ mediaId: String, localUrl: String) -> MediaEntry {
+        return assetsManager.createLocalMediaEntry(for: mediaId, localURL: URL(string: localUrl)!)
+    }
+    
+/************************/
+// MARK: - Actions
+/***********************/
+    
+    @IBAction func playTouched(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        if !(player.isPlaying) {
+            self.playheadTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { (timer) in
+                self.playheadSlider.value = Float(player.currentTime / player.duration)
+            })
+            
+            player.play()
+        }
+    }
+    
+    @IBAction func pauseTouched(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        self.playheadTimer?.invalidate()
+        self.playheadTimer = nil
+        player.pause()
+    }
+    
+    @IBAction func playheadValueChanged(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        let slider = sender as! UISlider
+        
+        print("playhead value:", slider.value)
+        player.currentTime = player.duration * Double(slider.value)
+    }
+}
