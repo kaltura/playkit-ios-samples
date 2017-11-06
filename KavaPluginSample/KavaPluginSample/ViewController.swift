fffa87192e4187796c80071c5b5e048a726dc47f
Eliza Sapir <eliza.sapir@kaltura.com>
2017-11-06 15:10:39 +0200

    kava plugin sample attachment

---
+++ KavaPluginSample/KavaPluginSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KavaPluginSample
+//
+//  Created by Eliza Sapir on 29/10/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
