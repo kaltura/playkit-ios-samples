a9dbd43d916d933a564ce11d7900014dd51ec717
Vadim Kononov <vadim.kononov@kaltura.com>
2017-11-06 13:21:16 +0200

    FEM-1713: change event name

--- TracksSample/BasicSample/ViewController.m
+++ TracksSample/BasicSample/ViewController.m
@@ -80,7 +80,7 @@
     __weak __typeof(self) weakSelf = self;
     
     // add observer to tracksAvailable event
-    [self.player addObserver:self events:@[PlayerEvent.tracksAvailable, PlayerEvent.trackSelected] block:^(PKEvent * _Nonnull event) {
+    [self.player addObserver:self events:@[PlayerEvent.tracksAvailable, PlayerEvent.trackChanged] block:^(PKEvent * _Nonnull event) {
         if ([event isKindOfClass:PlayerEvent.tracksAvailable]) {
             // Connect data
             weakSelf.picker.dataSource = weakSelf;
@@ -97,7 +97,7 @@
             if (event.tracks.textTracks) {
                 weakSelf.textTracks = event.tracks.textTracks;
             }
-        } else if ([event isKindOfClass:PlayerEvent.trackSelected]) {
+        } else if ([event isKindOfClass:PlayerEvent.trackChanged]) {
             NSLog(@"selected track:: %@", event.selectedTrack.title);
         }
     }];
