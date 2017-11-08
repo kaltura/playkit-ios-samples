1a9f525ef0127dffd9ebc54b7492ea2c973e2ce8
Vadim Kononov <vadim.kononov@kaltura.com>
2017-11-08 13:26:24 +0200

    FEM-1676: player with empty kava plugin config added to sample

--- KavaPluginSample/KavaPluginSample/AppDelegate.swift
+++ KavaPluginSample/KavaPluginSample/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import PlayKitKava
+import PlayKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        PlayKitManager.shared.registerPlugin(KavaPlugin.self)
         return true
     }
 
--- KavaPluginSample/KavaPluginSample/ViewController.swift
+++ KavaPluginSample/KavaPluginSample/ViewController.swift
@@ -1,25 +1,116 @@
 //
 //  ViewController.swift
-//  KavaPluginSample
+//  BasicSample-Swift
 //
-//  Created by Eliza Sapir on 29/10/2017.
+//  Created by Eliza Sapir on 12/04/2017.
 //  Copyright © 2017 Kaltura. All rights reserved.
 //
 
 import UIKit
+import PlayKit
+import PlayKitKava
+
+/*
+ This sample will show you how to create a player with basic functionality.
+ The steps required:
+ 1. Load player with plugin config (only if has plugins).
+ 2. Register player events.
+ 3. Prepare Player.
+ */
 
 class ViewController: UIViewController {
-
+    var player: Player?
+    var playheadTimer: Timer?
+    @IBOutlet weak var playerContainer: PlayerView!
+    @IBOutlet weak var playheadSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.playheadSlider.isContinuous = false;
+        
+        // 1. Load the player
+        do {
+            let kavaConfig = KavaPluginConfig.init(uiconfId: -1, partnerId: -1, ks: "-", playbackContext: "-", referrerAsBase64: "-", baseUrl: "-", customVar1: "-", customVar2: "-", customVar3: "-")
+            let pluginConfig = PluginConfig(config: [KavaPlugin.pluginName: kavaConfig])
+            self.player = try PlayKitManager.shared.loadPlayer(pluginConfig: pluginConfig)
+            // 2. Register events if have ones.
+            // Event registeration must be after loading the player successfully to make sure events are added,
+            // and before prepare to make sure no events are missed (when calling prepare player starts buffering and sending events)
+            
+            // 3. Prepare the player (can be called at a later stage, preparing starts buffering the video)
+            self.preparePlayer()
+        } catch let e {
+            // error loading the player
+            print("error:", e.localizedDescription)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    /************************/
+    // MARK: - Player Setup
+    /***********************/
+    func preparePlayer() {
+        // setup the player's view
+        self.player?.view = self.playerContainer
+        
+        let contentURL = "https://cdnapisec.kaltura.com/p/2215841/playManifest/entryId/1_w9zx2eti/format/applehttp/protocol/https/a.m3u8"
+        
+        // create media source and initialize a media entry with that source
+        let entryId = "sintel"
+        let source = PKMediaSource(entryId, contentUrl: URL(string: contentURL), drmData: nil, mediaFormat: .hls)
+        // setup media entry
+        let mediaEntry = PKMediaEntry(entryId, sources: [source], duration: -1)
+        
+        // create media config
+        let mediaConfig = MediaConfig(mediaEntry: mediaEntry)
+        
+        // prepare the player
+        self.player!.prepare(mediaConfig)
+    }
+    
+    /************************/
+    // MARK: - Actions
+    /***********************/
+    
+    @IBAction func playTouched(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        if !(player.isPlaying) {
+            self.playheadTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { (timer) in
+                self.playheadSlider.value = Float(player.currentTime / player.duration)
+            })
+            
+            player.play()
+        }
+    }
+    
+    @IBAction func pauseTouched(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        self.playheadTimer?.invalidate()
+        self.playheadTimer = nil
+        player.pause()
+    }
+    
+    @IBAction func playheadValueChanged(_ sender: Any) {
+        guard let player = self.player else {
+            print("player is not set")
+            return
+        }
+        
+        let slider = sender as! UISlider
+        
+        print("playhead value:", slider.value)
+        player.currentTime = player.duration * Double(slider.value)
+    }
 }
-
