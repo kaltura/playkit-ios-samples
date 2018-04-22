88cba5d6a35eb469e3cddae8d55c2359eb08eb69
Nilit Danan <nilit.danan@kaltura.com>
2018-04-22 10:35:59 +0300

    WIP

---
+++ AVAssetDownloadSample/AVAssetDownloadSample/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVAssetDownloadSample
+//
+//  Created by Nilit Danan on 4/18/18.
+//  Copyright © 2018 Kaltura Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
