8d79740773976eecb968505e311739245e13577d
Noam Tamim <noam.tamim@kaltura.com>
2018-10-11 22:48:07 +0300

    Use released PlayKit

--- OVPStarter/OVPStarter/AppDelegate.swift
+++ OVPStarter/OVPStarter/AppDelegate.swift
@@ -10,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        PKLog.enabled = false
         PlayKitManager.shared.registerPlugin(KavaPlugin.self)
         return true
     }
