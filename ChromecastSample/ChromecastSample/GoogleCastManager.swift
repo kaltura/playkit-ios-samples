ba1121af3b623ea123dd14e80ced7ff8ad9619c4
Nilit Danan <nilit.danan@kaltura.com>
2018-10-16 06:45:37 +0300

    FEM-2103 Updated to a different ad. Added the change to the podfile.lock file.

--- ChromecastSample/ChromecastSample/GoogleCastManager.swift
+++ ChromecastSample/ChromecastSample/GoogleCastManager.swift
@@ -108,7 +108,7 @@ class GoogleCastManager: NSObject,
             .set(audioLanguage: "en")
             .set(textLanguage: "en")
             .set(adTagType: .vmap)
-            .set(adTagURL: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostlongpod&cmsid=496&vid=short_tencue&correlator=[timestamp]")
+            .set(adTagURL: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=[timestamp]")
             .build()
         
         return media
