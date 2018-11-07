bc0fcf30e3f4a11cfc24957879968b3dc46b685f
Nilit Danan <nilit.danan@kaltura.com>
2018-11-07 10:00:41 +0200

    EventsRegistration: Removed pre_install and post_install in pod file. pod update. Updated to Swift 4.2. Added replay button. Clean up and updated code.

--- EventsRegistration/EventsRegistration-Swift/AppDelegate.swift
+++ EventsRegistration/EventsRegistration-Swift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PlayKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        PlayKitManager.logLevel = .debug
+        
         return true
     }
 
