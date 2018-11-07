017ed1ef2232567e3f54a539fd9d8cac79f0bc0f
Nilit Danan <nilit.danan@kaltura.com>
2018-11-07 10:25:12 +0200

    KalturaLiveStatsSample: Removed pre_install and post_install in pod file. pod update. Updated to Swift 4.2.

--- KalturaLiveStatsSample/KalturaLiveStatsSample-Swift/AppDelegate.swift
+++ KalturaLiveStatsSample/KalturaLiveStatsSample-Swift/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         PlayKitManager.shared.registerPlugin(KalturaLiveStatsPlugin.self)
         return true
     }
