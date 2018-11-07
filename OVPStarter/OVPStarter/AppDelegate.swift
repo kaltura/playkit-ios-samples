ef2e042b282af1fc30b2b7db6c9332af50d326c8
Nilit Danan <nilit.danan@kaltura.com>
2018-11-07 11:06:43 +0200

    OVPStarter: Updated the pod file. pod update. Updated to Swift 4.2.

--- OVPStarter/OVPStarter/AppDelegate.swift
+++ OVPStarter/OVPStarter/AppDelegate.swift
@@ -9,8 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        PKLog.enabled = false
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        PKLog.enabled = false
         PlayKitManager.shared.registerPlugin(KavaPlugin.self)
         return true
     }
