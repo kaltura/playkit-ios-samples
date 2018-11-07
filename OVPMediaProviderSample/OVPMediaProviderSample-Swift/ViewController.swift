d0973684b6be520b7c53bbebb7797c283a43e793
Nilit Danan <nilit.danan@kaltura.com>
2018-11-07 10:56:07 +0200

    OVPMediaProviderSample: Removed pre_install and post_install in pod file. pod update. Updated to Swift 4.2.

--- OVPMediaProviderSample/OVPMediaProviderSample-Swift/ViewController.swift
+++ OVPMediaProviderSample/OVPMediaProviderSample-Swift/ViewController.swift
@@ -62,7 +62,7 @@ class ViewController: UIViewController {
         let serverURL = "https://cdnapisec.kaltura.com"
         let partnerId: Int64 = 1424501 // put your partner id here
         // in real app you will need to provide a ks if your app need it, if not keep empty for anonymous session.
-        let sessionProvider = SimpleOVPSessionProvider(serverURL:serverURL, partnerId:partnerId, ks:nil )
+        let sessionProvider = SimpleSessionProvider(serverURL:serverURL, partnerId:partnerId, ks:nil )
         let mediaProvider: OVPMediaProvider = OVPMediaProvider(sessionProvider)
         mediaProvider.entryId = "1_djnefl4e"
         mediaProvider.loadMedia { (mediaEntry, error) in
